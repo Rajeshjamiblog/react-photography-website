3a6b1ea31a9fffc4a9052452abb16aa9baa6c1b3
Rajeshjamiblog <rajeshjamiblog@gmail.com>
2026-08-02 00:42:27 +0530

    Add website branding configuration

---
+++ src/config/siteConfig.js
@@ -0,0 +1,12 @@
+const siteConfig = {
+    siteName: "Artsy by Sam",
+
+    title: "Artsy by Sam | Professional Photography",
+
+    description:
+        "Artsy by Sam captures beautiful wedding, portrait and event moments.",
+
+    logoText: "Artsy by Sam"
+};
+
+export default siteConfig;
