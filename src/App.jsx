4256769367f9ff7648c41d6a3f4f724a90f290aa
Rajeshjamiblog <rajeshjamiblog@gmail.com>
2026-08-02 01:27:06 +0530

    Create home page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Hero from "./components/Hero/Hero";
-import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+
 
 function App() {
 
   return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
+    <Home /> 
   );
 
 }
